d69f6b32b38ff026dde73077a360eed5738933fa
shizuka takada <takada.a4t@gmail.com>
2018-08-13 14:13:41 +0000

    index画面を修正

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -1,6 +1,7 @@
 class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
-    mail to: User.find_by(id: picture.user_id).email, subject: "新規投稿完了メール"
+    @user = User.find_by(id: @picture.user_id)
+    mail to: @user.email, subject: "新規投稿完了メール"
   end
 end
